030949168c0406a6115da06547040a27d57a2252
David Archer <16766645+davidaustinarcher@users.noreply.github.com>
2023-03-24 10:57:06 +0000

    Removing EKS

--- terraform-eks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "initials" {
-  description = "Enter your initials to include in URLs. Lowercase only!!!"
-  default     = ""
-}
-
-variable "region" {
-  description = "The AWS region where all resources in this example should be created"
-  default     = ""
-}
